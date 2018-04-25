3e862ee229d65f9142501b9319106f7d9fbc298f
machengallen <machengallen@163.com>
2018-04-25 13:25:09 +0800

    【摘要】jpa依赖工具包提交

---
+++ jpa-util/src/main/java/com/iv/jpa/util/hibernate/HibernateCallBack.java
@@ -0,0 +1,16 @@
+package com.iv.jpa.util.hibernate;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+
+/**
+ * 回调接口
+ * @author macheng
+ *
+ */
+public interface HibernateCallBack {
+
+	Object doInHibernate(Session ses) throws HibernateException;
+
+}
---
+++ jpa-util/src/main/java/com/iv/jpa/util/hibernate/HibernateUtil.java
@@ -0,0 +1,69 @@
+package com.iv.jpa.util.hibernate;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 数据库初始化工具类
+ * 
+ * @author macheng
+ *
+ */
+@Component("hibernateUtil")
+public class HibernateUtil {
+
+	private static SessionFactory factory;
+
+	/*
+	 * static { try { Configuration cfg = new Configuration().configure();
+	 * ServiceRegistry sr = new StandardServiceRegistryBuilder()
+	 * .applySettings(cfg.getProperties()).build(); factory =
+	 * cfg.buildSessionFactory(sr); } catch (HibernateException e) { // TODO
+	 * Auto-generated catch block e.printStackTrace(); }
+	 * 
+	 * }
+	 */
+	@Autowired
+	public HibernateUtil(EntityManagerFactory emf) {
+		super();
+		if (emf.unwrap(SessionFactory.class) == null) {
+
+			throw new NullPointerException("factory is not a hibernate factory");
+		}
+		HibernateUtil.factory = emf.unwrap(SessionFactory.class);
+	}
+
+	/*
+	 * @Autowired public void setFactory(EntityManagerFactory emf) {
+	 * 
+	 * if(emf.unwrap(SessionFactory.class) == null){
+	 * 
+	 * throw new NullPointerException("factory is not a hibernate factory"); }
+	 * HibernateUtil.factory = emf.unwrap(SessionFactory.class); }
+	 */
+	public static Session getSession(String tenantId) {
+		if (null != tenantId) {
+			return factory.withOptions().tenantIdentifier(tenantId).openSession();
+		}
+		return factory.openSession();
+	}
+
+	public static void close(Session ses) {
+		if (ses != null) {
+			ses.close();
+		}
+	}
+
+	public static SessionFactory getFactory() {
+		return factory;
+	}
+
+	public static void setFactory(SessionFactory factory) {
+		HibernateUtil.factory = factory;
+	}
+
+}
