57266274349c36689026497a46883d8378e2ca75
machengallen <machengallen@163.com>
2018-06-04 10:53:19 +0800

    【摘要】脚本库内部服务接口提交

--- script-service-api/src/main/java/com/iv/script/api/service/IScriptService.java
+++ script-service-api/src/main/java/com/iv/script/api/service/IScriptService.java
@@ -1,5 +1,29 @@
 package com.iv.script.api.service;
 
+import java.io.InputStream;
+
 public interface IScriptService {
 
+	/**
+	 * 存储临时文件
+	 * @param inputStream
+	 * @return
+	 */
+	int tempWrite(String fileName, InputStream inputStream);
+	
+	/**
+	 * 获取临时文件流
+	 * @param scriptId
+	 * @return
+	 */
+	InputStream tempRead(int scriptId);
+	
+	/**
+	 * 获取正式脚本库文件流
+	 * @param scriptId
+	 * @return
+	 */
+	InputStream officialRead(int scriptId);
+	
+	
 }
