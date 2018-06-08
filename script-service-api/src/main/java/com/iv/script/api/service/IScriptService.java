850acfbfa12a8445c36ffc433422e0c50efe647a
machengallen <machengallen@163.com>
2018-06-08 09:51:24 +0800

    略

--- script-service-api/src/main/java/com/iv/script/api/service/IScriptService.java
+++ script-service-api/src/main/java/com/iv/script/api/service/IScriptService.java
@@ -13,43 +13,48 @@ public interface IScriptService {
 
 	/**
 	 * 存储临时文件
+	 * 
 	 * @param inputStream
 	 * @return
 	 */
 	@RequestMapping(value = "/temp/write", method = RequestMethod.GET)
-	int tempWrite(@RequestParam("fileName") String fileName, @RequestParam("inputStream") InputStream inputStream);
-	
+	int tempWrite(@RequestParam("fileName") String fileName, @RequestParam("type") String type,
+			@RequestParam("inputStream") InputStream inputStream);
+
 	/**
 	 * 获取临时文件流
+	 * 
 	 * @param scriptId
 	 * @return
 	 */
 	@RequestMapping(value = "/temp/read", method = RequestMethod.GET)
 	InputStream tempRead(@RequestParam("scriptId") int scriptId);
-	
+
 	/**
 	 * 获取正式脚本库文件流
+	 * 
 	 * @param scriptId
 	 * @return
 	 */
 	@RequestMapping(value = "/official/read", method = RequestMethod.GET)
 	InputStream officialRead(@RequestParam("scriptId") int scriptId);
-	
+
 	/**
 	 * 查询脚本库文件信息
+	 * 
 	 * @param scriptId
 	 * @return
 	 */
 	@RequestMapping(value = "/script/info", method = RequestMethod.GET)
 	ScriptDto scriptInfoById(@RequestParam("scriptId") int scriptId);
-	
+
 	/**
 	 * 查询临时库脚本文件信息
+	 * 
 	 * @param scriptId
 	 * @return
 	 */
 	@RequestMapping(value = "/temporaryScript/info", method = RequestMethod.GET)
 	TemporaryScriptDto temporaryScriptInfoById(@RequestParam("scriptId") int scriptId);
-	
-	
+
 }
