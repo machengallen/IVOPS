51927f70facf54d9af82e41acd96b2be5cf064e4
zhangzhang <zhangy@inno-view.cn>
2018-06-13 15:20:18 +0800

    【功能摘要】：权限相关dto

---
+++ user-service-api/src/main/java/com/iv/outer/dto/SubTenantRoleDto.java
@@ -0,0 +1,21 @@
+package com.iv.outer.dto;
+
+public class SubTenantRoleDto {
+	private int id;
+	/*角色名称*/
+	private String name;	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+		
+	
+}
