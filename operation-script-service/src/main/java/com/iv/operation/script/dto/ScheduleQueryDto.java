aa3603e87372e10683f3d28e737daa13123d84ca
machengallen <machengallen@163.com>
2018-06-26 15:01:18 +0800

    略

--- operation-script-service/src/main/java/com/iv/operation/script/dto/ScheduleQueryDto.java
+++ operation-script-service/src/main/java/com/iv/operation/script/dto/ScheduleQueryDto.java
@@ -6,4 +6,29 @@ public class ScheduleQueryDto {
 	private int items;
 	private Integer id;// 作业id
 	private String taskName;// 作业任务名
+	public int getCurPage() {
+		return curPage;
+	}
+	public void setCurPage(int curPage) {
+		this.curPage = curPage;
+	}
+	public int getItems() {
+		return items;
+	}
+	public void setItems(int items) {
+		this.items = items;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTaskName() {
+		return taskName;
+	}
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+	
 }
