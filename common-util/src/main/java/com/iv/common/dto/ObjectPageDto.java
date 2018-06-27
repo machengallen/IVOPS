5ff34ac7737cf2c7b710b1c2b4a3fc35c5efbb51
machengallen <machengallen@163.com>
2018-06-27 19:29:46 +0800

    【功能摘要】 ObjectPageDto

--- common-util/src/main/java/com/iv/common/dto/ObjectPageDto.java
+++ common-util/src/main/java/com/iv/common/dto/ObjectPageDto.java
@@ -2,20 +2,20 @@ package com.iv.common.dto;
 
 import java.util.Collection;
 
-public class ObjectPageDto {
+public class ObjectPageDto<T> {
 
 	private long total;
-	private Collection<Object> data;
+	private Collection<T> data;
 	public long getTotal() {
 		return total;
 	}
 	public void setTotal(long total) {
 		this.total = total;
 	}
-	public Collection<Object> getData() {
+	public Collection<T> getData() {
 		return data;
 	}
-	public void setData(Collection<Object> data) {
+	public void setData(Collection<T> data) {
 		this.data = data;
 	}
 	
