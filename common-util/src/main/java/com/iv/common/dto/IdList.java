4a11f603c6aaa23ff583aef8b0610fc17f4f1f4a
machengallen <machengallen@163.com>
2018-06-29 09:25:40 +0800

    【摘要】新增批量id dto类

---
+++ common-util/src/main/java/com/iv/common/dto/IdList.java
@@ -0,0 +1,17 @@
+package com.iv.common.dto;
+
+import java.util.List;
+
+public class IdList<T> {
+
+	private List<T> ids;
+
+	public List<T> getIds() {
+		return ids;
+	}
+
+	public void setIds(List<T> ids) {
+		this.ids = ids;
+	}
+	
+}
